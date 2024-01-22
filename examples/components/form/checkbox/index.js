eddfea382398560449da0b864c065b9d21ea2d45
kossyak <akosyak@protonmail.com>
2024-01-22 18:47:46 +0400

    checkbox toggle button

--- examples/components/form/checkbox/index.js
+++ examples/components/form/checkbox/index.js
@@ -7,12 +7,21 @@ const root = document.querySelector('#root')
 const app = createApp()
 
 const component = {
-  template: `<div class="example1"></div>`,
+  template: `<div class="example1"></div><button class="btn">toggle</button>`,
+  proxies: {
+    value: false
+  },
   nodes() {
     return {
+      btn: {
+        onclick: () => this.proxy.value = !this.proxy.value
+      },
       example1: {
         component: {
-          src: checkbox
+          src: checkbox,
+          proxies: {
+            value: () => this.proxy.value
+          }
         }
       }
     }
