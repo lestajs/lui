decd5a6fb18651bacd6acc509a5e5bad1685e683
MaksonesKirb <90245223+MaksonesKirb@users.noreply.github.com>
2024-02-03 20:23:46 +0400

    Update index.js
    Обновлённый пример с аккордеонами.
    
    Содержит в себе:
    — 3 аккордеона, которые закрывают друг друга при открытии.
    — 1 аккордеон с кнопками, меняющие disabled и value

--- examples/components/accordion/index.js
+++ examples/components/accordion/index.js
@@ -8,9 +8,23 @@ const root = document.querySelector('#root')
 const app = createApp()
 
 const component = {
-  template: `<div class="example1"></div><div class="btn"></div>`,
+  template: `
+  <div class="example1"></div>
+  <div class="example2"></div>
+  <div class="example3"></div>
+  <hr>
+  <div class="example4"></div>
+  <div class="l-fx l-gap">
+    <div class="btn1"></div>
+    <div class="btn2"></div>
+  </div>
+  `,
   proxies: {
-    value: false,
+    value1: true,
+    value2: true,
+    value3: true,
+    value4: true,
+    disabled: false
   },
   nodes () {
     return {
@@ -18,33 +32,124 @@ const component = {
         component: {
           src: accordion,
           proxies: {
-            value: () => this.proxy.value
+            value: () => this.proxy.value1
           },
           params: {
-            name: '',
-            text: 'Accordion'
+            name: 'about',
+            text: 'About'
           },
           methods: {
-            action: (n, v) => this.proxy.value = !this.proxy.value
+            action: (n, v) => {
+              this.proxy.value1 = !this.proxy.value1
+              this.proxy.value2 = this.proxy.value3 = true
+            }
           },
           sections: {
             content: {
               src: {
-                template: `Basically, the first child css will target the first paragraph element inside the parent tag, and the rest of the paragraph element will be ignored, that is, if we add styling with the first child selector for the paragraph tag, only the first paragraph tag will be styled, and the rest of the paragraph tag will not be ...`
+                template: `It is an component Accordion of framework Lesta`
               }
             }
           }
         }
       },
-      btn: {
+      example2: {
+        component: {
+          src: accordion,
+          proxies: {
+            value: () => this.proxy.value2
+          },
+          params: {
+            name: 'cont',
+            text: 'Contacts'
+          },
+          methods: {
+            action: (n, v) => {
+              this.proxy.value1 = this.proxy.value3 = true
+              this.proxy.value2 = !this.proxy.value2
+            }
+          },
+          sections: {
+            content: {
+              src: {
+                template: `+7-800-555-35-35`
+              }
+            }
+          }
+        }
+      },
+      example3: {
+        component: {
+          src: accordion,
+          proxies: {
+            value: () => this.proxy.value3
+          },
+          params: {
+            name: 'faq',
+            text: 'FAQ'
+          },
+          methods: {
+            action: (n, v) => {
+              this.proxy.value1 = this.proxy.value2 = true
+              this.proxy.value3 = !this.proxy.value3
+            }
+          },
+          sections: {
+            content: {
+              src: {
+                template: `There are all FAQs`
+              }
+            }
+          }
+        }
+      },
+      example4: {
+        component: {
+          src: accordion,
+          proxies: {
+            value: () => this.proxy.value4,
+            disabled: () => this.proxy.disabled
+          },
+          params: {
+            name: 'test',
+            text: 'Test change'
+          },
+          methods: {
+            action: (n, v) => {
+              this.proxy.value4 = !this.proxy.value4
+            }
+          },
+          sections: {
+            content: {
+              src: {
+                template: `With this accordion you can use the rest of the functionality of this component.`
+              }
+            }
+          }
+        }
+      },
+      btn1: {
         component: {
           src: button,
           proxies: {
-            value: () => this.proxy.value ? 'Open' : 'Close'
+            value: () => this.proxy.disabled ? 'Enabled' : 'Disabled'
           },
           methods: {
             action: () => {
-              this.proxy.value = !this.proxy.value
+              this.proxy.disabled = !this.proxy.disabled
+            }
+          }
+        }
+      },
+      btn2: {
+        component: {
+          src: button,
+          proxies: {
+            value: () => this.proxy.value4 ? 'Open' : 'Close'
+          },
+          methods: {
+            action: () => {
+              this.proxy.value4 = !this.proxy.value4
             }
           }
         }
